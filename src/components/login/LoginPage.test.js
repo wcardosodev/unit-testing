c2d52942e808cdbec3c1f16aa595203378503a6a
willaay96 <willaay96@hotmail.com>
2020-07-23 16:34:15 +0100

    Added Snapshot test

--- src/components/login/LoginPage.test.js
+++ src/components/login/LoginPage.test.js
@@ -1,3 +1,4 @@
+const renderer = require('react-test-renderer');
 const { login, getUser } = require('./LoginPage');
 
 test('Test login', async () => {
@@ -15,3 +16,31 @@ test('Get user', async () => {
 
     expect(user1).toEqual({username: 'jimmy', password: 'password'});
 });
+
+// Snapshot testing
+it('create random user', () => {
+    const user = {
+        createdAt: new Date(),
+        id: Math.floor(Math.random() * 20)
+    };
+
+    expect(user).toMatchSnapshot({
+        createdAt: expect.any(Date),
+        id: expect.any(Number)
+    });
+});
+
+it('will fail every time', () => {
+    // Creates it with initial values for example
+    // createdAt = july20th2020
+    // id = 12
+    // when the first test is run the values will be the same and this will pass
+    // however that snapshot is then saved, so each next time u do the test the values won't be the same and it would need to be updated
+    const user = {
+      createdAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+      name: 'LeBron James',
+    };
+  
+    expect(user).toMatchSnapshot();
+  });
