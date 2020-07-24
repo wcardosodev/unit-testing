ff549f11ee6708ea368bc36c8ad619188ca45751
willaay96 <willaay96@hotmail.com>
2020-07-24 15:22:35 +0100

    Using Enzyme for testing simulations

---
+++ src/components/enzyme-counterApp/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState} from 'react';
+
+export const Counter = props => {
+    const [counter, setCounter] = useState(0);
+
+    const increment = () => {
+        setCounter(counter + 1);
+    };
+
+    const decrement = () => {
+        // Only decrement if counter is greater than 0
+        if (counter === 0) return;
+        setCounter(counter - 1);
+    }
+
+    return (
+        <div className='app'>
+            <h1>Counter</h1>
+            <div id='counter-text'>{counter}</div>
+            <button className='btn-inc' onClick={increment}>Increment</button>
+            <button className='btn-dec' onClick={decrement}>Decrement</button>
+        </div>
+    );
+};
